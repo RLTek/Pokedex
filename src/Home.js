8ed73943e337939a732ce3463ffcd7490b0c40cd
RL Tek <Rlusby@live.com>
2023-03-02 13:55:18 -0500

    added feature to filter by generation

--- src/Home.js
+++ src/Home.js
@@ -10,6 +10,10 @@ export function Home(){
     const [search, setSearch] = useState("");
     const [type, setType] = useState("all")
     const [pokemonTypes, setPokemonTypes] = useState({})
+    const [generation, setGeneration] = useState("all")
+    const [generationName, setGenerationName] = useState({})
+
+    
 
     const navigate = useNavigate();
 
@@ -27,14 +31,26 @@ export function Home(){
     .then(j =>setPokemonTypes(j))
   }, [type])
 
+//calls the API if a generation is selected
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/generation/${generation}`)
+    .then(k => k.json())
+    .then(k =>setGenerationName(k))
+  }, [generation])
+
 //maps through the results of the pokemon API being called and lists each pokemon
     const pokemonList = pokemon.results?.map(i => <div>
       <Link to={`/pokemon/${i.name}`}><h3>{i.name}</h3></Link>
     </div>)
 
-//maps through the reulse of the pokemonTypes if a type is selected
+//maps through the results of the pokemonTypes if a type is selected
     const typeList = pokemonTypes.pokemon?.map(j => <div>
       <Link to={`/pokemon/${j.pokemon.name}`}><h3>{j.pokemon.name}</h3></Link>
+    </div>)
+
+//maps through the resuls of generation if a generation is selected
+    const generationList = generationName.pokemon_species?.map(k => <div>
+      <Link to={`/pokemon/${k.name}`}><h3>{k.name}</h3></Link>
     </div>)
 
 
@@ -45,24 +61,11 @@ export function Home(){
       
     }
     
-  
-  
-  
-  
-  console.log(pokemon)
-      
 
-  return (
-    <div className="App">
-      <h1>Pokedex</h1>
-
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={search} onChange ={e => setSearch(e.target.value.toLowerCase())}/>
-      <input type="submit" value="search" />
-    </form>
-
-      <select value={type} onChange={e => setType(e.target.value)}>
-      <option>all</option>
+//JSX for dropdown filter when Type is selected
+    const selectByType = (
+      <select value={type} onChange={e => setType( e.target.value)}>
+        <option>all</option>
         <option>normal</option>
         <option>fighting</option>
         <option>flying</option>
@@ -83,12 +86,70 @@ export function Home(){
         <option>fairy</option>
         <option>unknown</option>
         <option>shadow</option>
-        
       </select>
+    )
 
+    const generationNames =(
+      <select value={generation} onChange={e => setGeneration(e.target.value)}>
+        <option>all</option>
+        <option>generation-i</option>
+        <option>generation-ii</option>
+        <option>generation-iii</option>
+        <option>generation-iv</option>
+        <option>generation-v</option>
+        <option>generation-vi</option>
+        <option>generation-vii</option>
+        <option>generation-viii</option>
+        <option>generation-ix</option>
+      </select>
+    )
+
+
+//Handles switching between the different filter types
+    const typeChoice = document.getElementById('type-choice')
+    const generationChoice = document.getElementById('generation-choice')
+    const [filter, setFilter] = useState("type")
+
+    const handleChange = () => {
+     if(typeChoice.checked){
+      setFilter("type")
+     }
+     else if(generationChoice.checked){
+      setFilter("generation")
+     }
+    } 
+
+  
+
+  return (
+    <div className="App">
+      <h1>Pokedex</h1>
+
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={search} onChange ={e => setSearch(e.target.value.toLowerCase())}/>
+      <input type="submit" value="search" />
+    </form>
+
+    <div onChange={handleChange}>
+      <p>Filter by:</p>
+      <label for="type">Type</label>
+      <input type="radio" id="type-choice" value="type" name="filterBy" defaultChecked></input>
+      <span> or </span>
+      <label for="generation">Generation</label>
+      <input type="radio" id="generation-choice" value="generation" name="filterBy"></input>
+    </div>
+
+    <div id="filter-choice">
+     {filter === "type" ? selectByType : generationNames}
+
+    </div>
+      
+    
       <div id="poke-list">
-        {type === "all" ? pokemonList : typeList}
+        {filter === "type" && type === "all" ? pokemonList : filter === "type" && type !== "all" ? typeList : filter === "generation" && generation === "all" ? pokemonList : filter === "generation" && type !== "all" ? generationList : typeList} 
+        
       </div>
     </div>
   );
+
 }
